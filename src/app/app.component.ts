8abf75dbf6177d8f4ab93c364679ba1bf7655c84
Danish Suri <dsuri@ncsu.edu>
2018-01-11 14:57:25 +0530

    Added new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,6 @@
 import { Component } from '@angular/core';
 
-export class Invention
-{
-  name: String;
-  inventor: String;
-  year: String;
-}
+
 
 @Component({
   selector: 'app-root',
@@ -15,9 +10,5 @@ export class Invention
 export class AppComponent {
   title = 'Inventions Hub';
 
-  invention:Invention ={
-    name: 'C Programming Language',
-    inventor:'Dennis Ritchie',
-    year: '1972'
-  }
+ 
 }
